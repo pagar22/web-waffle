084d30a14bfcfcbca30f0231dc61bb90a48dafd8
pagar22 <aaryanpagar@hotmail.com>
2023-05-14 20:22:10 +0200

    refactor: pages views

---
+++ src/pages/clubs.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Clubs() {
+  return <h1>We're gonna build our Clubs FE today</h1>;
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Clubs() {
+  return <h1>Sewy Seven</h1>;
+}
