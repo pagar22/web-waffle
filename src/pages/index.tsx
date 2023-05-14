8ddb8997036eb313af3cabbcae0dd27ce01d53a0
pagar22 <aaryanpagar@hotmail.com>
2023-05-15 00:48:33 +0200

    feat: basic FE

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
+import { Link, Stack } from "@mui/material";
+
 export default function Home() {
-  return <h1>Sewy Seven</h1>;
+  return (
+    <Stack alignItems={"center"}>
+      <h1>Sewy Seven</h1>
+      <Stack direction={"row"} spacing={2}>
+        <Link href={"/seasons"}>Seasons</Link>
+        <Link href={"/leagues"}>Leagues</Link>
+        <Link href={"/clubs"}>Clubs</Link>
+        <Link href={"/players"}>Players</Link>
+      </Stack>
+      <img
+        style={{ marginTop: 50 }}
+        src={
+          "https://media.tenor.com/ka5zqWWnutMAAAAC/cristiano-ronaldo-shocked.gif"
+        }
+        alt={"SEWY"}
+      />
+    </Stack>
+  );
 }
